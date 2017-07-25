7cdcb21e97690d7d0a32f6dc07c71f938925b547
Niranjan Molkeri <nmolkeri@gmail.com>
2017-07-24 20:00:26 -0700

    card section

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import React , { Component } from 'react';
-import { View , Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 class App extends Component {
+  componentWillMount(){
+      firebase.initializeApp({
+      apiKey: "AIzaSyBU2AtpijI69-Ns2DcZBMjETiecv0IfZqY",
+      authDomain: "authentication-44412.firebaseapp.com",
+      databaseURL: "https://authentication-44412.firebaseio.com",
+      projectId: "authentication-44412",
+      storageBucket: "authentication-44412.appspot.com",
+      messagingSenderId: "945539204645"
+    });
+  }
   render() {
     return (
       <View>
         <Header headerText="Authentication"/>
-        <Text>App front</Text>
+        <LoginForm />
       </View>
     );
   }
